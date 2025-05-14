00799d32de6e26ed2514284c1c196fbcf70b5d49
Najla Yaser <najlayasm@gmail.com>
2025-05-15 01:56:51 +0300

    init

--- frontend/Gharsa.py
+++ frontend/Gharsa.py
@@ -42,34 +42,81 @@ with open(welcome_icon_path, "rb") as f:
 
 
 
+# st.markdown("""
+#     <style>
+#     button[kind="primary"] {
+#         all: unset;
+#         text-decoration: none;
+#         color: #333;
+#         padding: 8px 15px;
+#         border-radius: 6px;
+#         font-family: 'Marhey', sans-serif;
+#       }
+#       button[kind="primary"] {
+#         all: unset;
+#         background-color: rgba(139,94,60,0.1);
+#         color: #8b5e3c;
+#         font-family: 'Marhey', sans-serif;
+#       }
+#     .st-key-nav {
+#         display: flex;
+#         justify-content: center;
+#         gap: 40px;
+#         background-color: rgba(255,255,255,0.8);
+#         padding: 15px;
+#         font-family: 'Marhey', sans-serif;
+#         font-size: 22px;
+#         direction: rtl;
+#         border-radius: 10px;
+#         box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+#     }
+#     .navbar {
+#         display: flex;
+#         justify-content: center;
+#         gap: 40px;
+#         background-color: rgba(255,255,255,0.8);
+#         padding: 15px;
+#         font-family: 'Marhey', sans-serif;
+#         font-size: 22px;
+#         direction: rtl;
+#         border-radius: 10px;
+#         box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+#     }
+#     .navbar a {
+#         text-decoration: none;
+#         color: #333;
+#         padding: 8px 15px;
+#         border-radius: 6px;
+#     }
+#     .navbar a:hover {
+#         background-color: rgba(139,94,60,0.1);
+#         color: #8b5e3c;
+#     }
+#     .active-nav {
+#         color: #8b5e3c !important;
+#         border-bottom: 2px solid #8b5e3c;
+#     }
+#     </style>
+#     <div class="navbar">
+#         <a href="/Gharsa" onclick="window.location.href='/Gharsa'" target="_self" class="active-nav">الرئيسية</a>
+#         <a href="/Plants_info" onclick="window.location.href='/Plants_info'" target="_self">تعرف على النباتات</a>
+#         <a href="/what_is_the_plant" onclick="window.location.href='/what_is_the_plant'" target="_self">ماهي نبتتي؟</a>
+#         <a href="/Plant_your_plant" onclick="window.location.href='/Plant_your_plant'" target="_self">ازرع نبتتك</a>
+#         <a href= "/Check_your_plant"onclick="window.location.href='/Check_your_plant'" target="_self">افحص نبتتك</a>
+#         <a href="/Team_members" onclick="window.location.href='/Team_members'" target="_self">الأعضاء</a>
+#     </div>
+# """, unsafe_allow_html=True)        
+
+# con =st.container(key="nav")
+# c1, c2, c3 = con.columns(3, gap="large")
+# with c1:
+#     st.button("افحص نبتتك", type="primary", key="Check_your_plant")
+# with c2:
+#     st.button("ازرع نبتتك", type="primary", key="Plant_your_plant")
+# with c3:  
+#     st.button("ماهي نبتتي؟", type="primary", key="what_is_the_plant")
 st.markdown("""
     <style>
-    button[kind="primary"] {
-        all: unset;
-        text-decoration: none;
-        color: #333;
-        padding: 8px 15px;
-        border-radius: 6px;
-        font-family: 'Marhey', sans-serif;
-      }
-      button[kind="primary"] {
-        all: unset;
-        background-color: rgba(139,94,60,0.1);
-        color: #8b5e3c;
-        font-family: 'Marhey', sans-serif;
-      }
-    .st-key-nav {
-        display: flex;
-        justify-content: center;
-        gap: 40px;
-        background-color: rgba(255,255,255,0.8);
-        padding: 15px;
-        font-family: 'Marhey', sans-serif;
-        font-size: 22px;
-        direction: rtl;
-        border-radius: 10px;
-        box-shadow: 0 2px 10px rgba(0,0,0,0.1);
-    }
     .navbar {
         display: flex;
         justify-content: center;
@@ -98,24 +145,14 @@ st.markdown("""
     }
     </style>
     <div class="navbar">
-        <a href="/Gharsa" onclick="window.location.href='/Gharsa'" target="_self" class="active-nav">الرئيسية</a>
-        <a href="/Plants_info" onclick="window.location.href='/Plants_info'" target="_self">تعرف على النباتات</a>
-        <a href="/what_is_the_plant" onclick="window.location.href='/what_is_the_plant'" target="_self">ماهي نبتتي؟</a>
-        <a href="/Plant_your_plant" onclick="window.location.href='/Plant_your_plant'" target="_self">ازرع نبتتك</a>
-        <a href= "/Check_your_plant"onclick="window.location.href='/Check_your_plant'" target="_self">افحص نبتتك</a>
-        <a href="/Team_members" onclick="window.location.href='/Team_members'" target="_self">الأعضاء</a>
+        <a href="/Gharsa" target="_self" class="active-nav">الرئيسية</a>
+        <a href="/Plants_info" target="_self">تعرف على النباتات</a>
+        <a href="/what_is_the_plant" target="_self">ماهي نبتتي؟</a>
+        <a href="/Plant_your_plant" target="_self">ازرع نبتتك</a>
+        <a href="/Check_your_plant" target="_self">افحص نبتتك</a>
+        <a href="/Team_members" target="_self">الأعضاء</a>
     </div>
-""", unsafe_allow_html=True)        
-
-con =st.container(key="nav")
-c1, c2, c3 = con.columns(3, gap="large")
-with c1:
-    st.button("افحص نبتتك", type="primary", key="Check_your_plant")
-with c2:
-    st.button("ازرع نبتتك", type="primary", key="Plant_your_plant")
-with c3:  
-    st.button("ماهي نبتتي؟", type="primary", key="what_is_the_plant")
-
+""", unsafe_allow_html=True)
 
 icons = {
     "detect": "detect_plant.png",
